237a2e1093bf5b371711524dd25c638c27124065
Trevor Phippard <trevorphippard@Lemonade-Stand.local>
2025-07-31 00:57:05 -0400

    1-ckms-001-initial-auth add login playwright test

--- frontend/e2e/home.spec.ts
+++ frontend/e2e/home.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from "playwright/test"
 
 test("Home page", async ({ page }) => {
-  await page.goto("https://practicesoftwaretesting.com/")
+  await page.goto("https://practicesoftwaretesting.com/");
+  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+
 })
